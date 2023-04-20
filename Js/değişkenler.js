a4419ab6c0284ad4ed683c20dd975b01a02c4a93
Betmen20 <batu5729@outlook.com>
2023-04-20 03:36:32 +0300

    değişkenler

---
+++ Js/değişkenler.js
@@ -0,0 +1,45 @@
+let batuHarçlık = 500;
+var ömerHarçlık = 300;
+let furkanHarçlık = 200;
+
+var artış = 0.4;
+
+console.log(batuHarçlık + (batuHarçlık*artış))
+console.log(ömerHarçlık + (ömerHarçlık*artış))
+console.log(furkanHarçlık + (furkanHarçlık*artış))
+
+
+let isim = "Batu"; //string
+var yaşadığışehir = 34; //number
+var memleket = 37;
+
+console.log(typeof isim);
+console.log(typeof memleket);
+console.log(yaşadığışehir + memleket);
+
+
+let zenginmi = (maaşı > 50000);
+var maaşı=8000;
+
+console.log(zenginmi);
+console.log(typeof zenginmi); //boolean
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var ve let : değişken atamak için kullanılır
+// console.log(); : ekrana yansıtmak için kullanılır
+
+// string: yazı türünde cevap verir
+// number: sayı türünde cevap verir
+// boolean: true/false türünde cevap verir
