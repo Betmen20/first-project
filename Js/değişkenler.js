c68201e98decbee1e3dde186af59a696a347bfdb
Betmen20 <batu5729@outlook.com>
2023-05-26 01:48:38 +0300

    değişkenler

--- Js/değişkenler.js
+++ Js/değişkenler.js
@@ -1,20 +1,20 @@
-// let batuhanHarçlık = 500;
-// var ömerHarçlık = 300;
-// let berkanHarçlık = 200;
+let batuhanHarçlık = 500;
+var ömerHarçlık = 300;
+let berkanHarçlık = 200;
 
-// var artış = 0.4;
+var artış = 0.4;
 
-// console.log(batuhanHarçlık + (batuhanHarçlık*artış))
-// console.log(ömerHarçlık + (ömerHarçlık*artış))
-// console.log(berkanHarçlık + (berkanHarçlık*artış))
+console.log(batuhanHarçlık + (batuhanHarçlık*artış))
+console.log(ömerHarçlık + (ömerHarçlık*artış))
+console.log(berkanHarçlık + (berkanHarçlık*artış))
 
-// let isim = "Batuhan"; //String ("") varsa yazı
-// var yaşadığışehir = 34; //Number ("") yoksa sayı
-// var memleket = 37;
+let isim = "Batuhan"; //String ("") varsa yazı
+var yaşadığışehir = 34; //Number ("") yoksa sayı
+var memleket = 37;
 
-// console.log(typeof isim);
-// console.log(typeof memleket);
-// console.log(yaşadığışehir + memleket);
+console.log(typeof isim);
+console.log(typeof memleket);
+console.log(yaşadığışehir + memleket);
 
 
 
@@ -23,24 +23,6 @@ var maaşı=8000;
 
 console.log(zenginmi);
 console.log(typeof zenginmi);//boolean
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // var ve let : değişken atamak için kullanılır
 // console.log(); : ekrana yansıtmak için kullanılır
