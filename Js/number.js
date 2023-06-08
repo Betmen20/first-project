2d4d20f67d31e4d40b3e6c40a2243a868410097c
Betmen20 <batu5729@outlook.com>
2023-06-08 17:34:44 +0300

    number

---
+++ Js/number.js
@@ -0,0 +1,29 @@
+var sonuç
+
+
+sonuç = 5;
+sonuç = "5";
+sonuç = String(5);
+sonuç = parseFloat("5.4");
+sonuç = parseInt("5.9");
+
+let sayısı = 5.4461561561456;
+
+sonuç = sayısı.toFixed(5);
+sonuç = sayısı.toPrecision(5);
+
+
+
+sonuç =  Math.round(5.6);
+sonuç = Math.floor(5.4);
+sonuç = Math.min(3,6,1,8,0,5,9,);
+sonuç = Math.max(3,6,1,8,0,5,9,);
+sonuç = Math.random();
+sonuç = Math.ceil(6.4);
+sonuç = Math.sqrt(64);
+
+
+
+
+console.log(sonuç);
+console.log(typeof sonuç);
