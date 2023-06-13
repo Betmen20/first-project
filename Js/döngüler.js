bc9e456bda963d0ce2c7726271c30ed6339da4f8
Betmen20 <batu5729@outlook.com>
2023-06-13 15:32:14 +0300

    döngüler

---
+++ Js/döngüler.js
@@ -0,0 +1,52 @@
+// Object üzerinde döngü...
+// let ülke ={
+//     "isim" : "Türkiye",
+//     "baskent" : "Ankara",
+//     "enkalabalıksehri" : "İstanbul",
+//     "nüfus" : 83
+// }
+
+// for(let key in ülke){
+    
+//     console.log(key);
+//     console.log(ülke[key]);
+
+// }
+
+// Temel Döngüler...
+var sayılar =[1,3,5,7,9,2,3,5,1,45,23,34,12];
+let toplam = 0;
+
+for(let sayı of sayılar){
+    toplam += sayı;
+
+}
+
+// for(let a=0;a < sayılar.length; a++){
+
+//     toplam += sayılar[a];
+// }
+
+// for(let a in sayılar){
+//     toplam += sayılar[a];
+
+// }
+
+console.log(toplam);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for : Döngü Yapmamızı Sağlar.
+// for(){
+
+// }
